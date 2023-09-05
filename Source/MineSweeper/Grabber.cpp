54f04747db0c2ad3d02005398570f02d500f1e3d
Mieszko <gaskamiesz@gmail.com>
2023-09-05 19:05:42 +0200

    Fixed character movement

---
+++ Source/MineSweeper/Grabber.cpp
@@ -0,0 +1,146 @@
+#include "Grabber.h"
+
+#define OUT
+
+// Sets default values
+UGrabber::UGrabber()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FindPhysicsHandle();
+	SetupInputComponent();
+}
+
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	FVector PlayerViewLocation = GetPlayerLocation();
+	FRotator PlayerViewRotation = GetPlayerRotation();
+
+	FVector LineTraceEnd = PlayerViewLocation + PlayerViewRotation.Vector() * m_Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+
+	if(!PhysicsHandle)
+		return;
+
+	if (PhysicsHandle->GrabbedComponent) {
+		UE_LOG(LogTemp, Warning, TEXT("WE HAVE IT: %s !"), *GetOwner()->GetActorLocation().ToString());
+		PhysicsHandle->SetTargetLocation(GetPlayerReach());
+	}
+}
+
+void UGrabber::FindPhysicsHandle()
+{
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (PhysicsHandle == nullptr)
+		UE_LOG(LogTemp, Error, TEXT("No physics handle component included to object: %s !"), *GetOwner()->GetName());
+}
+
+void UGrabber::SetupInputComponent()
+{
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (InputComponent)
+	{
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+	}
+}
+
+void UGrabber::Grab()
+{
+	FHitResult HitResult = GetFirstPhysicsBodyInReach();
+	UPrimitiveComponent* ComponentToGrab = HitResult.GetComponent();
+
+	AActor* ActorHit = HitResult.GetActor();
+	if (ActorHit)
+	{
+		if (PhysicsHandle)
+			return;
+		PhysicsHandle->GrabComponentAtLocation(ComponentToGrab, NAME_None, GetPlayerReach());
+	};
+}
+
+void UGrabber::Release()
+{
+	if (PhysicsHandle->GrabbedComponent)
+	{
+		if (!PhysicsHandle)
+			return;
+		PhysicsHandle->ReleaseComponent();
+	}
+}
+
+FVector UGrabber::GetPlayerLocation() const
+{
+	FVector PlayerViewLocation;
+	FRotator PlayerViewRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewLocation, OUT PlayerViewRotation);
+
+	return PlayerViewLocation;
+}
+
+FRotator UGrabber::GetPlayerRotation() const
+{
+	FVector PlayerViewLocation;
+	FRotator PlayerViewRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewLocation, OUT PlayerViewRotation);
+
+	return PlayerViewRotation;
+}
+
+FVector UGrabber::GetPlayerReach() const
+{
+	FVector PlayerViewLocation = GetPlayerLocation();
+	FRotator PlayerViewRotation = GetPlayerRotation();
+
+	FVector LineTraceEnd = PlayerViewLocation + PlayerViewRotation.Vector() * m_Reach;
+
+	UE_LOG(LogTemp, Warning, TEXT("Actor location: %s"), *LineTraceEnd.ToString());
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0,
+		m_Reach
+	);
+
+	return LineTraceEnd;
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
+{
+	FHitResult Hit;
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());;
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		GetPlayerLocation(),
+		GetPlayerReach(),
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+
+	return Hit;
+}
---
+++ Source/MineSweeper/Grabber.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "Grabber.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class MINESWEEPER_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UGrabber();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	// sets debug line length
+	float m_Reach = 125.f;
+
+	UPROPERTY()
+		UPhysicsHandleComponent* PhysicsHandle = nullptr;
+
+	UPROPERTY()
+		UInputComponent* InputComponent = nullptr;
+
+	// Checking for Physics Handle Component. Protection from nullptr
+	void FindPhysicsHandle();
+	void SetupInputComponent();
+	void Grab();
+	void Release();
+
+	// Return the Player position in world
+	FVector GetPlayerLocation() const;
+
+	// Return the Player rotation in world
+	FRotator GetPlayerRotation() const;
+
+	// Return the Line Trace End
+	FVector GetPlayerReach() const;
+
+	// Return the first Actor within reach with physics body
+	FHitResult GetFirstPhysicsBodyInReach() const;
+
+};
